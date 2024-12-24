0fb1b604053302f6ea50980fa73c9cdb40915a16
Mitchell Christ <mitchell@human.marketing>
2024-12-23 17:18:57 -0800

    update URL

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV === 'development'
 
-export const BASE_URL = dev ? 'http://localhost:3000' : 'https://svg.vercel.app'
+export const BASE_URL = dev
+	? 'http://localhost:3000'
+	: 'https://icon-urls.vercel.app'
